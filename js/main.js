3f8abe9c4c1f2e44919114d1d2defc1fbcb251d2
Johan Sangalang <jnsangalang93@yahoo.com>
2024-04-30 23:04:48 +0000

    created part of retrieveMonsterInformation function, retrieving monster data with use of API, still need to continue function to display pulled data

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 "use strict";
-let $monsterSearchBar = document.querySelector('.monster-search-bar');
+let $formInput = document.querySelector('#form-input');
+let $monsterInput = document.querySelector('.monster-input');
 let $monsterSearchButton = document.querySelector('.monster-search-button');
 let $view = document.querySelectorAll('.view');
 let $homeButton = document.querySelector('.home-button');
+let $monsterInformation = document.querySelector('.monster-information');
+let $submitMonsterButton = document.querySelector('.submit-monster-button');
 const domQueries = {
-    $monsterSearchBar,
+    $formInput,
+    $monsterInput,
     $monsterSearchButton,
     $view,
+    $homeButton,
+    $monsterInformation,
+    $submitMonsterButton
 };
 for (const key in domQueries) {
     if (!domQueries[key])
@@ -14,12 +21,25 @@ for (const key in domQueries) {
 }
 // const $icon = document.createElement('i');
 // $icon.setAttribute('class','fa-solid fa-dice-d20');
-// $monsterSearchBar?.append($icon);
+// $monsterInput?.append($icon);
 $monsterSearchButton?.addEventListener('click', () => {
     viewSwap('monster-view');
 });
 $homeButton?.addEventListener('click', () => {
     viewSwap('home-view');
+});
+$submitMonsterButton?.addEventListener('click', (event) => {
+    event.preventDefault();
+    const $formElement = $formInput.elements;
+    const monsterName = {
+        name: $formElement.name.value,
+    };
+    if (monsterName.name) {
+        retrieveMonsterInformation(monsterName.name);
+    }
+    else {
+        $monsterInformation.textContent = "Please use a monster name";
+    }
 });
 function viewSwap(string) {
     for (let i = 0; i < $view.length; i++) {
@@ -32,3 +52,47 @@ function viewSwap(string) {
         }
     }
 }
+async function retrieveMonsterInformation(monsterName) {
+    const response = await fetch('https://www.dnd5eapi.co/api/monsters');
+    $monsterInformation.textContent = "Looking up...";
+    const responseMonsters = await response.json();
+    const monstersInfo = responseMonsters.results;
+    console.log(monstersInfo);
+    try {
+        for (let i = 0; i < monstersInfo.length; i++) {
+            if (monstersInfo[i].name.toLowerCase() === monsterName.toLowerCase()) {
+                const response = await fetch(`https://www.dnd5eapi.co${monstersInfo[i].url}`);
+                console.log(response);
+                const matchMonsterResponse = await response.json();
+                const monsterData = matchMonsterResponse;
+                console.log(monsterData);
+                const $monsterTitle = document.createElement('h2');
+                $monsterTitle.classList.add('.row');
+                $monsterTitle.textContent = monsterData.name;
+                const $monsterParagraph = document.createElement('p');
+                $monsterParagraph.classList.add('.row');
+                $monsterParagraph.textContent = monsterData.Index;
+                $monsterTitle.append($monsterParagraph);
+            }
+            else {
+                $monsterInformation.textContent = "Monster not found";
+            }
+        }
+    }
+    catch (error) {
+        if (!response.ok) {
+            const message = `Failed to get monsters, Error ${response.status}`;
+            $monsterInformation.textContent = "Error retrieving monster data";
+            throw new Error(message);
+        }
+    }
+}
+console.log(retrieveMonsterInformation('goblin'));
+// async function createMonster(){
+//   const response = await fetch('https://www.dnd5eapi.co/api/monsters');
+//   try{
+//     const monsters = await response.json();
+//     console.log(monsters);
+//   }
+// }
+// console.log(createMonster());
--- ts/main.ts
+++ ts/main.ts
@@ -1,21 +1,37 @@
-interface Spell{
-  name:string,
-  range:string,
-  school:string,
-  concentration:boolean,
-  castTime: string,
-  material:string
+interface FormElements extends HTMLFormControlsCollection{
+  name: HTMLInputElement
 }
 
-let $monsterSearchBar = document.querySelector('.monster-search-bar');
+interface MonsterName{
+  name:string
+}
+
+
+interface Spell{
+  name:string;
+  range:string;
+  school:string;
+  concentration:boolean;
+  castTime: string;
+  material:string;
+}
+
+let $formInput = document.querySelector('#form-input') as HTMLFormElement;
+let $monsterInput = document.querySelector('.monster-input') as HTMLInputElement;
 let $monsterSearchButton = document.querySelector('.monster-search-button');
 let $view = document.querySelectorAll('.view');
 let $homeButton = document.querySelector('.home-button');
+let $monsterInformation = document.querySelector('.monster-information') as HTMLDivElement;
+let $submitMonsterButton = document.querySelector('.submit-monster-button');
 
 const domQueries: Record<string,any> = {
-  $monsterSearchBar,
+  $formInput,
+  $monsterInput,
   $monsterSearchButton,
   $view,
+  $homeButton,
+  $monsterInformation,
+  $submitMonsterButton
 }
 
 for(const key in domQueries){
@@ -24,7 +40,7 @@ for(const key in domQueries){
 
 // const $icon = document.createElement('i');
 // $icon.setAttribute('class','fa-solid fa-dice-d20');
-// $monsterSearchBar?.append($icon);
+// $monsterInput?.append($icon);
 
 $monsterSearchButton?.addEventListener('click',()=>{
   viewSwap('monster-view');
@@ -32,6 +48,22 @@ $monsterSearchButton?.addEventListener('click',()=>{
 
 $homeButton?.addEventListener('click', ()=>{
   viewSwap('home-view');
+})
+
+$submitMonsterButton?.addEventListener('click',(event:Event)=>{
+  event.preventDefault();
+  const $formElement = $formInput.elements as FormElements;
+
+  const monsterName:MonsterName = {
+   name: $formElement.name.value,
+  };
+
+  if(monsterName.name){
+    retrieveMonsterInformation(monsterName.name);
+  }
+  else{
+    $monsterInformation.textContent = "Please use a monster name";
+  }
 })
 
 function viewSwap(string:string):void{
@@ -45,3 +77,60 @@ function viewSwap(string:string):void{
     }
   }
 }
+
+
+async function retrieveMonsterInformation(monsterName:string){
+  const response = await fetch('https://www.dnd5eapi.co/api/monsters');
+
+  $monsterInformation.textContent="Looking up...";
+    const responseMonsters = await response.json();
+    const monstersInfo = responseMonsters.results;
+    console.log(monstersInfo);
+
+  try {
+    for(let i = 0; i < monstersInfo.length ; i++){
+      if(monstersInfo[i].name.toLowerCase() === monsterName.toLowerCase()){
+
+        const response = await fetch(`https://www.dnd5eapi.co${monstersInfo[i].url}`);
+        console.log(response);
+
+        const matchMonsterResponse = await response.json();
+        const monsterData = matchMonsterResponse;
+
+        console.log(monsterData);
+
+        const $monsterTitle = document.createElement('h2');
+        $monsterTitle.classList.add('.row');
+        $monsterTitle.textContent = monsterData.name;
+
+        const $monsterParagraph = document.createElement('p');
+        $monsterParagraph.classList.add('.row');
+        $monsterParagraph.textContent = monsterData.Index;
+
+        $monsterTitle.append($monsterParagraph);
+
+      }
+      else {
+        $monsterInformation.textContent = "Monster not found";
+    }
+    }
+  }
+  catch(error){
+    if(!response.ok){
+      const message = `Failed to get monsters, Error ${response.status}`;
+      $monsterInformation.textContent = "Error retrieving monster data";
+      throw new Error(message);
+    }
+  }
+
+}
+console.log(retrieveMonsterInformation('goblin'));
+
+// async function createMonster(){
+//   const response = await fetch('https://www.dnd5eapi.co/api/monsters');
+//   try{
+//     const monsters = await response.json();
+//     console.log(monsters);
+//   }
+// }
+// console.log(createMonster());
